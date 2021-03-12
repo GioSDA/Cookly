c89b625ad8bed237844b19cecfee233a1fc9a5c4
GioSDA <gsdambrosio@gmail.com>
2021-03-11 21:08:42 -0800

    Spring Initialize

---
+++ src/test/java/ly/cook/cookly/CooklyApplicationTests.java
@@ -0,0 +1,13 @@
+package ly.cook.cookly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CooklyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
