89810bef8425e813e4454f311e2d34dbb8f917c5
GioSDA <gsdambrosio@gmail.com>
2021-03-25 19:04:54 -0700

    added vuetify carousel

---
+++ src/main/resources/static/js/app.js
@@ -0,0 +1,21 @@
+const vueApp = new Vue({
+    el: '#container',
+    data () {
+        return {
+            items: [
+                {
+                    src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg'
+                },
+                {
+                    src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg'
+                },
+                {
+                    src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg'
+                },
+                {
+                    src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg'
+                }
+            ]
+            }
+      }
+})
